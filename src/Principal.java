3645a7f9155d03b1cb00fa82f24a9f0189ed47fb
Vinicius Costa <54358510+ViniciusCosta126@users.noreply.github.com>
2024-02-20 18:12:59 -0300

    Complementando modelo de filme, com metodos privados

--- src/Principal.java
+++ src/Principal.java
@@ -1,17 +1,18 @@
+import br.com.vinicius.screenmatch.modelos.Filme;
+
 public class Principal {
     public static void main(String[] args) {
         Filme meuFilme = new Filme();
-        meuFilme.nome = "O poderoso chefão";
-        meuFilme.anoDeLancamento = 1970;
-        meuFilme.duracaoEmMinutos = 180;
+        meuFilme.setNome("O poderoso chefão");
+        meuFilme.setAnoDeLancamento(1970);
+        meuFilme.setDuracaoEmMinutos(180);
+        meuFilme.setIncluidoNoPlano(true);
 
         meuFilme.exibeFichaTecnica();
         meuFilme.avalia(8.5);
         meuFilme.avalia(9.1);
         meuFilme.avalia(5);
-
-        System.out.println(meuFilme.somaDasAvaliacoes);
-        System.out.println(meuFilme.totalDeAvaliacoes);
+        System.out.println(meuFilme.getTotalDeAvaliacoes());
         System.out.println(meuFilme.pegaMedia());
     }
 }
